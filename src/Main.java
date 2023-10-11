932990b1126e9edea40bb95d28b1e55b3b543215
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-10-10 22:06:58 -0300

    comecando so prints da tabuada2

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,8 @@ public class Main {
         }while (!ativa_proximo);
 
         Calculadora calc = new Calculadora();
-        int[] numeros = new int[] calc.print_index(tipo);
-        calc.print_results(numeros{0},numeros{1});
+        int[] numeros = calc.print_index(tipo);
+        calc.print_results(numeros[0],numeros[1]);
 
 
     }
